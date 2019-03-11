c07bf9c58eccd84f86fb567ff9741ff94ac596ea
Kostis Giampous <k.giabou@gmail.com>
2019-03-11 20:23:54 +0100

    Update Suitability maps over country boundaries.r

--- Suitability maps over country boundaries.r
+++ Suitability maps over country boundaries.r
@@ -18,3 +18,16 @@ p<- levelplot(Hsap_suit, margin=FALSE, main = list(paste0("H.sapiens_", sdm_bins
 cex=0.85), col.regions = colorRampPalette(c("grey90", "orange", "dark red"))(100)) + layer(sp.polygons(bPols, alpha=0.9))
 assign(paste("p_",sdm_bins[s], "k", sep=""), p)
 }
+
+## Plot in trellis device ##
+for (sk in seq_along(ls(pattern="_p")))
+{
+pp <- get(ls(pattern=paste("p_"))[13+sk])
+assign(paste0("p", sk, sep=""), pp)
+}
+
+trellis.device(device="pdf", file="H.sapiens_Ensemble_skata.pdf", width=15, height=12)
+print(p1, split=c(1,1,1,3), more=TRUE)
+print(p2, split=c(1,2,1,3), more=TRUE)
+print(p3, split=c(1,3,1,3), more=TRUE)
+dev.off()
